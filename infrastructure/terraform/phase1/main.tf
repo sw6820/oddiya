f3898c6e47e85db1a6e3d4fe54a93aa0aeaee2d8
sw6820 <52646313+sw6820@users.noreply.github.com>
2025-11-05 14:12:23 +0900

    fix: increase app server volume size to 30GB (AMI snapshot requirement)

--- infrastructure/terraform/phase1/main.tf
+++ infrastructure/terraform/phase1/main.tf
@@ -229,7 +229,7 @@ resource "aws_instance" "app_server" {
 
   root_block_device {
     volume_type           = "gp3"
-    volume_size           = 20
+    volume_size           = 30
     delete_on_termination = true
   }
 
